4823e9442a87ee91e6fa266621f11b42ed3b5193
Anthony Hsu <erwaman@gmail.com>
2020-05-21 16:17:11 -0400

    Fixed packages.sh script (updated link to libpqxx deb files)

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -106,8 +106,8 @@ install_linux() {
       ant
    #install libpqxx-6.2 manually
    apt-get -y install wget
-   wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-dev_6.2.4-4_amd64.deb
-   wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-6.2_6.2.4-4_amd64.deb
+   wget http://repo.atlantic.net/ubuntu/pool/universe/libp/libpqxx/libpqxx-dev_6.2.4-4_amd64.deb
+   wget http://repo.atlantic.net/ubuntu/pool/universe/libp/libpqxx/libpqxx-6.2_6.2.4-4_amd64.deb
    dpkg -i libpqxx-6.2_6.2.4-4_amd64.deb
    dpkg -i libpqxx-dev_6.2.4-4_amd64.deb
    rm libpqxx-6.2_6.2.4-4_amd64.deb
